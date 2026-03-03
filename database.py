ff9f8979830babaa23f9195257d5be18cb87fe56
Mallikarjun-0 <saimallikarjun9@gmail.com>
2026-03-03 12:26:34 +0530

    database.py refactoring done!

--- database.py
+++ database.py
@@ -1,23 +1,58 @@
-from typing import List, Optional
+from __future__ import annotations
 
-from models import User, Product
+from typing import Dict, Generic, Optional, Protocol, Tuple, TypeVar
+
+from models import Product, User
+
+
+class HasId(Protocol):
+    id: int
+
+
+T = TypeVar("T", bound=HasId)
+
+
+class InMemoryRepository(Generic[T]):
+    def __init__(self) -> None:
+        self._items: Dict[int, T] = {}
+
+    def save(self, item: T) -> None:
+        self._items[item.id] = item
+
+    def get(self, item_id: int) -> Optional[T]:
+        return self._items.get(item_id)
+
+    def list_all(self) -> Tuple[T, ...]:
+        return tuple(self._items.values())
+
+    def __len__(self) -> int:
+        return len(self._items)
 
 
 class Database:
-    def __init__(self):
-        self.users: List[User] = []
-        self.products: List[Product] = []
+    def __init__(self) -> None:
+        self._users: InMemoryRepository[User] = InMemoryRepository()
+        self._products: InMemoryRepository[Product] = InMemoryRepository()
 
     def save_user(self, user: User) -> None:
-        self.users.append(user)
+        self._users.save(user)
 
     def get_user(self, user_id: int) -> Optional[User]:
-        return next((u for u in self.users if u.id == user_id), None)
+        return self._users.get(user_id)
 
     def save_product(self, product: Product) -> None:
-        self.products.append(product)
+        self._products.save(product)
 
     def get_product(self, product_id: int) -> Optional[Product]:
-        return next((p for p in self.products if p.id == product_id), None)
+        return self._products.get(product_id)
+
+    @property
+    def users(self) -> Tuple[User, ...]:
+        return self._users.list_all()
+
+    @property
+    def products(self) -> Tuple[Product, ...]:
+        return self._products.list_all()
+
 
 db = Database()
